af9bae3d6263d5c3c4304c5df93963b3c5156657
seongminm <131761210+seongminm@users.noreply.github.com>
2024-02-20 09:03:38 +0900

    Update and rename Rect.cpp to 03_3_Rect.cpp

--- ch03/03_3_Rect.cpp
+++ ch03/03_3_Rect.cpp
@@ -4,8 +4,9 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	// 사각형의 위치와 크기 정보를 표현할 때 사용
-	// x, y, widht, height 멤버 변수를 가지고 있음
+	// 사각형의 위치와 크기 정보를 표현할 때 사용 (Pointer + Size)
+	// Rect(x, y, widht, height) 멤버 변수를 가지고 있음
+	
 	Rect rc1;					// rc1 = [0 x 0 from (0, 0)]
 	Rect rc2(10, 10, 60, 40);	// rc2 = [60 x 40 from (10, 10)]
 
